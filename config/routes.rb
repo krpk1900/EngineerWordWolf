75e217866570e49a7de720d5913c94430946c587
krpk1900 <krpk1900@gmail.com>
2023-02-24 00:46:45 +0900

    rubocopを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'views#top'
-  get 'share', to: 'views#share'
+  get 'share',   to: 'views#share'
   get 'waiting', to: 'views#waiting'
-  get 'play', to: 'views#play'
-  get 'vote', to: 'views#vote'
-  get 'result', to: 'views#result'
+  get 'play',    to: 'views#play'
+  get 'vote',    to: 'views#vote'
+  get 'result',  to: 'views#result'
 end
