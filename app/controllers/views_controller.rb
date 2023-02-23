98b3d083f103471e8583f6871cb28c2c4b563617
krpk1900 <krpk1900@gmail.com>
2023-02-24 00:44:48 +0900

    画面を作成

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,5 +1,15 @@
 class ViewsController < ApplicationController
+  def top; end
+
+  def share; end
+
   def waiting
     @connection_count = 1
   end
+
+  def play; end
+
+  def vote; end
+
+  def result; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root "views#waiting"
+  root 'views#top'
+  get 'share', to: 'views#share'
+  get 'waiting', to: 'views#waiting'
+  get 'play', to: 'views#play'
+  get 'vote', to: 'views#vote'
+  get 'result', to: 'views#result'
 end
